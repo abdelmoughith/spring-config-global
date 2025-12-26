35132cb673984799bd74ed01667de840df9f6118
Abdelmoughith Lemkhentar pc fedora <abdelmoughith2002@gmail.com>
2025-12-26 18:38:31 +0100

    pipeline changed

--- src/main/java/pack/springconfig/controllers/Rest.java
+++ src/main/java/pack/springconfig/controllers/Rest.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rest {
     @GetMapping
     public String index() {
-        return "Spring Config is running";
+        return "Spring Config is running\n, the following update to check if pipeline is running";
     }
 }
